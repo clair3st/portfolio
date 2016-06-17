529e0478acd2f6e7766a6a305703863d1950254d
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-16 18:47:46 -0700

    consolidated funfacts view and page view files

--- scripts/view/page-view.js
+++ scripts/view/page-view.js
@@ -53,6 +53,20 @@
 
   Country.fetchAll(pageView.renderIndexPage);
 
+  pageView.funFactsSection = function() {
+    var template = Handlebars.compile($('#continentStats-template').html());
+
+    Country.numWordsByContinent().forEach(function(stat) {
+      $('.continent-stats').append(template(stat));
+    });
+
+    $('.countries').text(Country.arrayAll.length);
+    $('.continents').text(Country.allContinents().length);
+  };
+
+  Country.fetchAll(pageView.funFactsSection);
+
+
   module.pageView = pageView;
 
 })(window);
