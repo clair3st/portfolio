f2d7a750018b5e5330ee215a81f1b2a5896eadc6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-22 12:58:18 -0700

    refactored request repos code for the github proxy

--- scripts/model/repos.js
+++ scripts/model/repos.js
@@ -4,16 +4,10 @@
   repos.allRepos = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/clair3st/repos' + '?per_page=10' + '&sort=updated',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function(data) {
-        console.log('data, ', data);
-        repos.allRepos = data;
-        callback();
-      }
-    });
+    $.get('/github/users/clair3st/repos' + '?per_page=10' + '&sort=updated')
+    .done(function(data) {
+      repos.allRepos = data;
+    }).done(callback);
   };
 
   repos.withTheAttribute = function(myAttr) {
