26e4f8e66944006658d28bea29c0c962883b22d5
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-23 16:13:06 -0700

    added a home route

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,4 +1,4 @@
-page('/', articleController.index);
+page('/', homeController.index);
+page('/portfolio', projectController.index);
 page('/about', aboutController.index);
-page('/funfacts', funFactsController.index);
 page();
